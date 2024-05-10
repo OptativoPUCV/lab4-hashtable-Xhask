ec24bf8f385e04ec26a771d01ef851a4cf4126c7
Xhask <164363195+Xhask@users.noreply.github.com>
2024-05-10 02:54:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -75,7 +75,9 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {    
     if (map == NULL || key == NULL) return ;
+    
     long posicion = hash(key, map->capacity);
+    
     while (map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL){
         if (is_equal(map->buckets[posicion]->key, key)){
             map->buckets[posicion]->key = NULL;
@@ -84,6 +86,7 @@ void eraseMap(HashMap * map,  char * key) {
             return;
         }
         posicion = (posicion + 1) % map->capacity;
+    }
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
